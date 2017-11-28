0c2b894e1ce7411c7a155179090d8ef89b4df4ee
Redcloud <drailanjohn123@gmail.com>
2017-11-28 13:43:23 +0800

    create the content in SLIDE_DATA

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Slides from '../components/Slides';
 
+const SLIDE_DATA = [
+  { text: 'Welcome to JobApp' },
+  { text: 'Set your location, then swipe away' }
+];
+
 class WelcomeScreen extends Component {
   render(){
     return (
-    <Slides />
+    <Slides data={SLIDE_DATA} />
     );
   }
 }
