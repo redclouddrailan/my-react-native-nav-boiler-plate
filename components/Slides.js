e6e68ace72fffae54687af159a76fab04ce70adb
Redcloud <drailanjohn123@gmail.com>
2017-11-28 14:38:05 +0800

    putting a button on the last slide

--- components/Slides.js
+++ components/Slides.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
+
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1){
+      return (
+        <Button
+          title="Game on!"
+          raised
+        />
+      );
+    }
+  }
 
   renderSlides(){
-    return this.props.data.map((slide) => {
+    return this.props.data.map((slide, index) => {
       return (
       <View
       style={[styles.slideStyle, {backgroundColor: slide.color}]}
-      key={slide.text}>
+      key={slide.text}
+      >
         <Text style={styles.slideText}>{slide.text}</Text>
+        {this.renderLastSlide(index)}
       </View>
     );
     });
