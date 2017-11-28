9bc8d4b331c6c200bf9b641998794fb5bfec0e02
Redcloud <drailanjohn123@gmail.com>
2017-11-28 10:34:56 +0800

    boiler plate first commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react';
+
+
+class MapScreen extends Component {
+  render(){
+    return (
+      <View>
+        <Text>MapScreen</Text>
+        <Text>MapScreen</Text>
+        <Text>MapScreen</Text>
+        <Text>MapScreen</Text>
+        <Text>MapScreen</Text>
+        <Text>MapScreen</Text>
+      </View>
+    );
+  }
+}
+
+
+export default MapScreen;
---
+++ screens/SettingScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react';
+
+
+class SettingScreen extends Component {
+  render(){
+    return (
+      <View>
+        <Text>SettingScreen</Text>
+        <Text>SettingScreen</Text>
+        <Text>SettingScreen</Text>
+        <Text>SettingScreen</Text>
+        <Text>SettingScreen</Text>
+        <Text>SettingScreen</Text>
+      </View>
+    );
+  }
+}
+
+
+export default SettingScreen;
