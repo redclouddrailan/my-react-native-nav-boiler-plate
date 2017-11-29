baae0e45a1738f2786263181e6e479fed5e0348c
Redcloud <drailanjohn123@gmail.com>
2017-11-29 14:58:23 +0800

    update on the loading screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
+import { AppLoading } from 'expo';
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
@@ -9,11 +10,28 @@ const SLIDE_DATA = [
 ];
 
 class WelcomeScreen extends Component {
+
+  state = { token: null }
+
+  async componentWillMount(){
+    let token = await AsyncStorage.getItem('fb_token');
+
+    if (token) {
+      this.props.navigation.navigate('map');
+      this.setState({ token });
+    }else {
+      this.setState({ token: false });
+    }
+  }
+
   onComplete = () => {
     this.props.navigation.navigate('auth')
   }
 
   render(){
+    if (this.state.token === null) {
+    return <AppLoading />;
+    }
     return (
     <Slides data={SLIDE_DATA} onComplete={this.onComplete} />
     );
